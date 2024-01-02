d799d1e16f4505b1d6a0002ed9ac8598ad65c5f5
Ahmed-Boukhallout <massoubouka5@gmail.com>
2024-01-02 09:12:27 +0100

    details further

--- app/productDetails/[id]/page.tsx
+++ app/productDetails/[id]/page.tsx
@@ -1,156 +1,163 @@
 "use client"
-import { useState,useEffect } from 'react';
-// import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import TopHeader from "../../home/TopHeader";
+import Footer from "../../home/Footer";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
 
-import axios from 'axios';
 interface Product {
-  ProductID: number;
-  Name: string;
-  ProductImage: string[];
-  Price: string;
-  Description: string;
-  Availability:string;
-  Quantity:number;
-
-}
-const ProductDetails: React.FC = () => {
-  const [inp, setInp] = useState<number>(0);
-  const [hoveredImage, setHoveredImage] = useState(null);
-  const [product, setProduct] = useState<Product>({});
-  const userId = localStorage.getItem('userId');
-  const handleImageHover = (imageSrc: string): void => {
-    setHoveredImage(imageSrc);
-  };
-  const addCart=(obj:object)=>{
-    axios.post("http://localhost:3000/api/cart/addCart",obj)
-    .then((res)=>{console.log(res)})
-    .catch((err)=>console.log(err))
-  }
-  const handleImageLeave = () => {
-    setHoveredImage(null);
-  };
-const ratingChanged = (newRating:number) => {
-  console.log(newRating)
- 
+  id: string;
+  product_name: string;
+  rate: string;
+  quantity: number;
+  price: number;
+  description: string;
+  image: string;
 }
 
-useEffect(() => {
-  var currentUrl = window.location.href;
-  var endPoint=currentUrl.split("/")
-  var i=endPoint[endPoint.length-1]
-  axios
-    .get<Product>(`http://localhost:3000/api/products/getOneProd/${i}`)
-    .then((res) => {
-      setProduct(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}, []);
+interface User {
+  id: string;
+  // Add other user-related properties if needed
+}
 
-console.log(product);
+interface ProductDetailsProps {
+  user: User;
+}
+
+const ProductDetails: React.FC<ProductDetailsProps> = ({ user }) => {
+  const [oneProduct, setOneProduct] = useState<Product | null>(null);
+  const [oneProdImg, setOneProdImg] = useState<string[]>([]);
+  const [idImg, setIdImg] = useState(0);
+  const router = useRouter();
+
+  const addFavorite = (obj: { userId: string; productId: string }) => {
+    axios.post("http://localhost:3000/api/BuyMeAll/favorite", obj)
+      .then((response) => {
+        const responseData = response.data.products;
+        // Handle the response data as needed
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  const extractImages = (str: string) => {
+    return str.split(',');
+  };
+
+  useEffect(() => {
+  
+    var currentUrl = window.location.href;
+    var endPoint=currentUrl.split("/")
+    var id=endPoint[endPoint.length-1]
+      axios.get(`http://localhost:3000/api/BuyMeAll/products/${id}`).then((result) => {
+        setOneProduct(result.data);
+        setOneProdImg(extractImages(result.data.image));
+      });
+    
+  }, []);
 
   return (
     <>
-<Navbar/>
-<div className="bg-white flex flex-row justify-center w-full">
-<div className="bg-white w-[1437px] h-[1077px] relative">
-<div className="absolute w-[500px] h-[600px] top-[274px] left-[342px] bg-secondary rounded-[4px] overflow-hidden">
-     <img className="absolute w-[446px] h-[546px]  left-[27px]"  src={hoveredImage || product.ProductImage&&product.ProductImage[3]} alt='Image0'  />
-    </div>
-    <div className="grid grid-cols-1   top-0 right-0">
-      {/* Map through images and display smaller images */}
-      {product.ProductImage && product.ProductImage.map((imageSrc, index) => (
-        <img
-          key={index}
-          src={imageSrc}
-          alt='Image'
-          className="w-40 h-40 cursor-pointer"
-          onMouseOver={() => handleImageHover(imageSrc)}
-          onMouseLeave={handleImageLeave}
-        />
-      ))}
-    </div>
-  
-   
-        
+      <div className="divP">
+        <TopHeader />
+        <div className="divProduct23">
+          <div className="divProduct28">
+            <div className="divProduct29">
+              <div className="columnP">
+                {oneProdImg.map((el, i) => (
+                  <div key={i} onClick={() => { setIdImg(i) }} className="divProduct30">
+                    <div className="divProduct31">
+                      <img
+                        loading="lazy"
+                        srcSet={el ? el : ""}
+                        className="imgProduct5"
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="columnProduct2">
+                <div className="divProduct35">
+                  <img
+                    loading="lazy"
+                    srcSet={oneProdImg[idImg]}
+                    className="imgProduct9"
+                  />
+                </div>
+              </div>
+              <div className="columnProduct3">
+                <div className="divProduct36">
+                  <div className="divProduct37">{oneProduct?.product_name}</div>
+                  <div className="divProduct38">
+                    <div className="divProduct39">
+                      {parseFloat(oneProduct?.rate?.toString() ?? '') && <Stack spacing={1}>
+                        <Rating name="half-rating" defaultValue={parseFloat(oneProduct?.rate?.toString() ?? '')} precision={0.5} readOnly />
+                      </Stack>}
+                      <div className="divProduct40">({oneProduct?.quantity})</div>
+                    </div>
+                    <div className="divProduct41">In Stock</div>
+                  </div>
+                  <div className="divProduct42">${oneProduct?.price}</div>
+                  <div className="divProduct43">{oneProduct?.description}</div>
+                  <div className="divProduct44" />
 
-    <div className="absolute top-[273px] left-[910px] font-heading-24px-semibold font-[number:var(--heading-24px-semibold-font-weight)] text-text-2 text-[length:var(--heading-24px-semibold-font-size)] tracking-[var(--heading-24px-semibold-letter-spacing)] leading-[var(--heading-24px-semibold-line-height)] whitespace-nowrap [font-style:var(--heading-24px-semibold-font-style)]">
-{product&&product.Name}
-</div>
-<div className="absolute top-[350px] left-[910px] font-heading-24px-regular font-[number:var(--heading-24px-regular-font-weight)] text-text-2 text-[length:var(--heading-24px-regular-font-size)] tracking-[var(--heading-24px-regular-letter-spacing)] leading-[var(--heading-24px-regular-line-height)] whitespace-nowrap [font-style:var(--heading-24px-regular-font-style)]">
-{product&& product.Price &&product.Price}DT
-        </div>
-<div className="inline-flex items-start gap-[16px] absolute top-[314px] left-[910px]">
-<div className="inline-flex items-start gap-[8px] relative flex-[0_0_auto]">
-<ReactStars className="!relative !flex-[0_0_auto]"
- count={5} 
- onChange={ratingChanged}
- size={24}
- color2={'#ffd700'}
- style={{'margin-left':'15px'}} />
-  <div className=" mb-10 relative w-fit mt-[9px] opacity-50 font-title-14px-regular font-[number:var(--title-14px-regular-font-weight)] text-text-2 text-[length:var(--title-14px-regular-font-size)] tracking-[var(--title-14px-regular-letter-spacing)] leading-[var(--title-14px-regular-line-height)] whitespace-nowrap [font-style:var(--title-14px-regular-font-style)]">
-              (150 Reviews)
-            </div>
-            </div>
-            <div className="inline-flex items-center gap-[16px] relative flex-[0_0_auto]">
-            <div className="relative w-fit mt-[9px] opacity-60 font-title-14px-regular font-[number:var(--title-14px-regular-font-weight)] text-button-1 text-[length:var(--title-14px-regular-font-size)] tracking-[var(--title-14px-regular-letter-spacing)] leading-[var(--title-14px-regular-line-height)] whitespace-nowrap [font-style:var(--title-14px-regular-font-style)]">
-              In Stock
-            </div>
-     </div>
-     </div>
-     <p className="absolute w-[373px] top-[398px] left-[910px] font-title-14px-regular font-[number:var(--title-14px-regular-font-weight)] text-black text-[length:var(--title-14px-regular-font-size)] tracking-[var(--title-14px-regular-letter-spacing)] leading-[var(--title-14px-regular-line-height)] [font-style:var(--title-14px-regular-font-style)]">
-          PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install &amp;
-          mess free removal Pressure sensitive.
-        </p>
-        <div className="flex w-[159px] h-[44px] items-start absolute top-[519px] left-[897px]">
-          <div className="relative w-[40px] h-[44px] rounded-[4px_0px_0px_4px] overflow-hidden border border-solid border-[#00000080]">
-            <CiSquareMinus onClick={()=>setInp(inp-1)}  className="!left-[8px] !absolute !w-[24px] !h-[24px] !top-[10px]" />
-          </div>
-          <div className="relative w-[80px] h-[44px] -ml-px border-t [border-top-style:solid] border-b [border-bottom-style:solid] border-[#00000080]">
-            <div className="absolute top-[7px] left-[34px] font-title-20px-medium font-[number:var(--title-20px-medium-font-weight)] text-text-2 text-[length:var(--title-20px-medium-font-size)] tracking-[var(--title-20px-medium-letter-spacing)] leading-[var(--title-20px-medium-line-height)] whitespace-nowrap [font-style:var(--title-20px-medium-font-style)]">
-              <input type="text" className="-ml-8 w-20 text-black" placeholder={inp} />
-            </div>
-          </div>
-          <div className="relative w-[41px] h-[44px] -ml-px bg-secondary-2 rounded-[0px_4px_4px_0px] overflow-hidden">
-            <CiSquarePlus onClick={()=>setInp(inp+1)} className="!left-[9px] !absolute !w-[24px] !h-[24px] !top-[10px]" color="black" />
-          </div>
-        </div>
-        <button onClick={()=>{addCart({NameCart:product.Name,CartImage:product.ProductImage[0],Price:product.Price,Quantity:product.Quantity,userUserID:userId})}} button="small" className="!absolute !left-[1072px] !top-[519px] bg-red rounded w-48 h-12 text-black"  hover={false}    >Buy Now</button>
-        <div className="absolute w-[399px] h-[180px] top-[603px] left-[897px] rounded-[4px] overflow-hidden border border-solid border-[#00000080]">
-          <div className="absolute w-[399px] h-px top-[90px] left-0 opacity-50">
-            <img className="absolute w-[399px] h-px -top-px left-0 object-cover" alt="Line" src="line-1.svg" />
-          </div>
-          <div className="inline-flex items-center gap-[16px] absolute top-[24px] left-[16px]">
-            <CiDeliveryTruck  className="!relative !w-[40px] !h-[40px]" />
-            <div className="inline-flex flex-col items-start gap-[8px] relative flex-[0_0_auto]">
-              <div className="relative w-fit mt-[-1.00px] font-title-16px-medium font-[number:var(--title-16px-medium-font-weight)] text-black text-[length:var(--title-16px-medium-font-size)] tracking-[var(--title-16px-medium-letter-spacing)] leading-[var(--title-16px-medium-line-height)] whitespace-nowrap [font-style:var(--title-16px-medium-font-style)]">
-                Free Delivery
+                  <div className="divProduct57">
+                    <div className="divProduct60" ><div className="buyBtn">Buy Now</div></div>
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/ec08a593d461e728db2a3e431df9e075d5f9912126c045dc62171b9ec6894644?"
+                      className="imgProduct14"
+                      onClick={() => {
+                        addFavorite(
+                          { userId: user.id, productId: oneProduct?.id || "" }
+                        );
+                      }}
+                    />
+                  </div>
+                  <div className="divProduct61">
+                    <div className="divProduct62">
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/b4550074645e51a42aca1953c2a07533a677b1016164bd0048a3a97ea8722833?"
+                        className="imgProduct15"
+                      />
+                      <div className="divProduct63">
+                        <div className="divProduct64">Free Delivery</div>
+                        <div className="divProduct65">
+                          Enter your postal code for Delivery Availability
+                        </div>
+                      </div>
+                    </div>
+                    <div className="divProduct66" />
+                    <div className="divProduct67">
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/90a8842bc3d9920ea195b68825a420da192397e7504a3a1fec5c41ab12315e5e?"
+                        className="imgProduct16"
+                      />
+                      <div className="divProduct68">
+                        <div className="divProduct69">Return Delivery</div>
+                        <div className="divProduct70">
+                          Free 30 Days Delivery Returns.{" "}
+                          <span style={{ textDecoration: "underline" }}>
+                            Details
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <p className="relative w-fit [font-family:'Poppins-Medium',Helvetica] font-medium text-black text-[12px] tracking-[0] leading-[18px] underline whitespace-nowrap">
-                Enter your postal code for Delivery Availability
-              </p>
-              </div>
-          </div>
-          <div className="inline-flex items-center gap-[16px] absolute top-[106px] left-[16px]">
-            <TfiReload  className="!relative !w-[40px] !h-[40px]" />
-            <div className="inline-flex flex-col items-start gap-[8px] relative flex-[0_0_auto]">
-              <div className="relative w-fit mt-[-1.00px] font-title-16px-medium font-[number:var(--title-16px-medium-font-weight)] text-black text-[length:var(--title-16px-medium-font-size)] tracking-[var(--title-16px-medium-letter-spacing)] leading-[var(--title-16px-medium-line-height)] whitespace-nowrap [font-style:var(--title-16px-medium-font-style)]">
-                Return Delivery
-              </div>
-              <p className="relative w-fit [font-family:'Poppins-Medium',Helvetica] font-medium text-black text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
-                <span className="font-title-12px-medium font-[number:var(--title-12px-medium-font-weight)] text-black text-[length:var(--title-12px-medium-font-size)] tracking-[var(--title-12px-medium-letter-spacing)] leading-[var(--title-12px-medium-line-height)] [font-style:var(--title-12px-medium-font-style)]">
-                  Free 30 Days Delivery Returns.{" "}
-                </span>
-                <span className="underline">Details</span>
-              </p>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-    </div>
-      
     </>
-);
-};
- export default ProductDetails;
+  );
+}
+
+export default ProductDetails;
